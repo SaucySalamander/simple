d40a242db9d66412fe3ef65eaf4685448bb8d8ae
SaucySalamander <26693281+SaucySalamander@users.noreply.github.com>
2021-09-08 21:52:02 -0500

    Added input file functionality

--- src/core/init.rs
+++ src/core/init.rs
@@ -1,22 +1,29 @@
 use std::fs::File;
 use std::io::Write;
 use crate::core::SimpleConfig;
+use clap::ArgMatches;
 
-pub fn init_simple() {
-    create_simple_directory();
+pub fn init_simple(matches: &ArgMatches) {
+    create_simple_repository(matches);
 }
 
-fn create_simple_directory() {
+fn create_simple_repository(matches: &ArgMatches) {
     let path = std::path::Path::new("./.simple/simple.toml");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).unwrap();
+
+    let mut file = File::create(path).unwrap();
+
+    if matches.is_present("file") {
+        println!("{}", matches.value_of("file").unwrap());
+    } else {
+        println!("Could not find input file");
+    }
 
     let simple_config = SimpleConfig {
         name: "test".to_string(),
         repos: None,
     };
-
-    let mut file = File::create(path).unwrap();
 
     let data = toml::to_string(&simple_config).unwrap();
 
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
-use clap::{App, SubCommand};
+use clap::{App, Arg, SubCommand};
 
 mod core;
 
 fn main() {
     let matches = App::new("Simple")
-        .subcommand(SubCommand::with_name("init"))
-        .about("Command to init simple repo in directory")
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("Command to init simple repo in directory")
+                .arg(
+                    Arg::with_name("file")
+                        .short("f")
+                        .required(false)
+                        .takes_value(true)
+                        .help("Optional file to initialize the simple repo with"),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("add")
+                .about("Command to add source repository to the simple repo"),
+        )
         .get_matches();
 
     match matches.subcommand_name() {
-        Some("init") => core::init::init_simple(),
+        Some("init") => core::init::init_simple(matches.subcommand_matches("init").unwrap()),
+        Some("add") => core::io::add_source_repository(),
         _ => panic!("Subcommand not found"),
     }
 }
@@ -17,9 +31,9 @@ fn main() {
 #[cfg(test)]
 mod tests {
 
-    use std::{fs, path::Path, process::Command};
-    use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
     use crate::core::SimpleConfig;
+    use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
+    use std::{fs, path::Path, process::Command};
 
     #[test]
     fn init_test_good_arg() {
@@ -27,6 +41,18 @@ mod tests {
 
         cmd.arg("init");
         cmd.assert().success();
+
+        cleanup();
+    }
+
+    #[test]
+    fn init_test_init_arg_with_file() {
+        let mut cmd = Command::cargo_bin("simple").unwrap();
+
+        cmd.arg("init").arg("-f").arg("temp.txt");
+        cmd.assert()
+            .success()
+            .stdout(predicates::str::is_match("^temp.txt\n$").unwrap());
 
         cleanup();
     }
